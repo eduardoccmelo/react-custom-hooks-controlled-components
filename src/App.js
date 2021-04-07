1b3d51a629bf19659cc0c631d0dc282c1ae841cc
Eduardo Melo <eduardoccmelo@gmail.com>
2021-04-07 18:36:46 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,67 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
   // Add states and eventHandlers here
+  // const [user, setUser] = useState({
+  //   firstName: { name },
+  //   lastName: surname,
+  //   age: age,
+  //   email: email,
+  //   newsletter: check,
+  // });
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [age, setAge] = useState("");
+  const [email, setEmail] = useState("");
+  const [check, setCheck] = useState(false);
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log("submitted");
+    console.log("!!! FORM SUBMITTED !!!");
+    // console.log(`Name: ${event.target.firstName.value}`);
+    // console.log(`Surname: ${event.target.lastName.value}`);
+    // console.log(`Age: ${event.target.age.value}`);
+    // console.log(`Email: ${event.target.email.value}`);
+    // console.log(`Checkbox checked: ${event.target.newsletter.checked}`);
+    setName("");
+    setSurname("");
+    setAge(0);
+    setEmail("");
+    setCheck(false);
+    const userInformation = {
+      name,
+      surname,
+      age,
+      email,
+      check,
+    };
+    console.log(userInformation);
+  }
+
+  function handleNameChange(event) {
+    setName(event.target.value);
+    // console.log(event.target.value);
+  }
+
+  function handleSurnameChange(event) {
+    setSurname(event.target.value);
+    // console.log(event.target.value);
+  }
+
+  function handleAgeChange(event) {
+    setAge(event.target.value);
+    // console.log(event.target.value);
+  }
+
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+    // console.log(event.target.value);
+  }
+
+  function handleCheckboxChange(event) {
+    setCheck(event.target.checked);
+    // console.log(event.target.checked);
   }
 
   return (
@@ -14,19 +70,45 @@ function App() {
       <h2>Enter user credentials below:</h2>
       <form onSubmit={handleSubmit}>
         <label htmlFor="firstName">Name:</label>
-        <input type="text" name="firstName" />
+        <input
+          onChange={handleNameChange}
+          type="text"
+          name="firstName"
+          value={name}
+        />
         <label htmlFor="lastName">Surname:</label>
-        <input type="text" name="lastName" />
+        <input
+          onChange={handleSurnameChange}
+          type="text"
+          name="lastName"
+          value={surname}
+        />
         <label htmlFor="age">Age:</label>
-        <input type="number" name="age" />
+        <input
+          onChange={handleAgeChange}
+          type="number"
+          name="age"
+          value={age}
+        />
         <label htmlFor="email">Email:</label>
-        <input type="email" name="email" />
+        <input
+          onChange={handleEmailChange}
+          type="email"
+          name="email"
+          value={email}
+        />
         <div className="newsletter">
-          <input type="checkbox" name="newsletter" />
+          <input
+            onChange={handleCheckboxChange}
+            type="checkbox"
+            name="newsletter"
+            checked={check}
+          />
           <label htmlFor="newsletter">
             Yes, I would like many more emails!
           </label>
         </div>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
